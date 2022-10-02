19bfe2c8be3c2d2acc24704deb41db2d28a5ae7b
RMSabe <69878824+RMSabe@users.noreply.github.com>
2022-10-02 20:58:11 -0300

    Update pb_params.h

--- pb_params.h
+++ pb_params.h
@@ -1,7 +1,11 @@
 #ifndef PB_PARAMS_H
 #define PB_PARAMS_H
 
-#define WAVE_FILE_DIR "/media/rafael-user/HDD2/Common/AudioFiles/WAVE/Mono/BoateAzul-Mono.wav"
+//Input File Directory.
+//Must not contain whitespaces!
+#define WAVE_FILE_DIR "/home/user/Music/AudioFile.wav"
+
+//Audio Output ID. Use "default" for system's default audio output.
 #define AUDIO_DEV "plughw:0,0"
 
 #endif
